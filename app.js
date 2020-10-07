ad3a95d707013e9e3caa267dafdb4896b8df499a
Jishi8088 <68326989+Jishi8088@users.noreply.github.com>
2020-10-07 10:55:56 +0530

    final commit

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require('express')
 const app = express()
 const mongoose  = require('mongoose')
 const PORT = process.env.PORT || 5000
-const {MONGOURI} = require('./config/keys')
-
-
 mongoose.connect('mongodb://localhost:27017/net',{
     useNewUrlParser:true,
     useUnifiedTopology: true
